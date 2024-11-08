87ea953be2149e0f3cd6015a182039c7574e25c0
oscarmatiasg <oscarmatiasgch@gmail.com>
2024-11-08 20:17:22 -0300

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
     <!-- Barra de navegación con enlaces a diferentes rutas -->
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/addtask">Agregar Tarea</router-link> 
+    <nav class="navbar">
+      <div class="navbar-container">
+        <a class="navbar-brand" href="#">
+          <span class="vue-icon">
+            <!-- Logo de Vue en SVG -->
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69" width="24" height="24">
+              <path fill="#42b883" d="M130.88,0l-130.88,0l130.88,226.69l130.88-226.69h-52.352l-78.528,135.658l-78.528-135.658h-52.352l130.88,226.69l130.88-226.69h-52.352z"/>
+              <path fill="#35495e" d="M0,0h52.352l78.528,135.658l78.528-135.658h52.352l-130.88,226.69l-130.88-226.69z"/>
+            </svg>
+          </span>
+        <router-link to="/" class="nav-link">Gestor de Tareas</router-link>
+
+        </a>
+        <ul class="navbar-links">
+          <li><router-link to="/addtask" class="nav-link">➕ Añadir Tarea</router-link></li>
+          <li><router-link to="/tasklist" class="nav-link">📝 Lista de Tareas</router-link></li>
+          <li><router-link to="/combinedview" class="nav-link">📋 Vista Combinada</router-link></li>
+        </ul>
+      </div>
     </nav>
     <!-- Aquí se cargará el componente correspondiente a la ruta seleccionada -->
     <router-view/>
@@ -18,18 +33,69 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #f8f9fa;
 }
 
-nav {
-  padding: 30px;
+.navbar {
+  background-color: #007bff;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-nav a {
+.navbar-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.navbar-brand {
+  color: #f8f9fa;
   font-weight: bold;
-  color: #2c3e50;
+  font-size: 18px;
+  margin-right: 20px;
+  text-decoration: none;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.vue-icon {
+  margin-right: 10px;
+}
+
+.navbar-links {
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0;
+}
+
+.nav-link {
+  color: #f8f9fa;
+  font-weight: bold;
+  text-decoration: none;
+  margin: 0 20px;
+}
+
+.nav-link.router-link-exact-active {
+  color: #a5d6a7;
+}
+
+@media (max-width: 768px) {
+  .navbar-container {
+    flex-direction: column;
+    align-items: center;
+  }
+  .navbar-brand {
+    margin-bottom: 10px;
+  }
+  .navbar-links {
+    flex-direction: column;
+  }
+  .nav-link {
+    margin: 10px 0;
+  }
 }
 </style>
